b7073fceab4a57d989e80d45ca228f8cee663019
Fernando Mendoza <fcmendoza02@gmail.com>
2012-02-06 00:23:34 -0600

    Updated Updater to use the config values for start and end dates.

--- Updater/DasUpdater.cs
+++ Updater/DasUpdater.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Configuration;
 
 namespace Updater {
     class DasUpdater {
@@ -10,7 +11,11 @@ namespace Updater {
             _repo = repo;
         }
         internal void Run() {
-            for (int year = 2007; year <= 2011; year++) {
+            int result = 0;
+            int startyear = int.TryParse(ConfigurationManager.AppSettings["StartYear"], out result) ? result : 2008;
+            int endyear = int.TryParse(ConfigurationManager.AppSettings["EndYear"], out result) ? result : 2012;
+
+            for (int year = startyear; year <= endyear; year++) {
                 for (int month = 1; month <= 12; month++) {
                     var from = new DateTime(year, month, 01);
                     Run(from, from.AddMonths(1).AddDays(-1));
